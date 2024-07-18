b359ffd40d195fbc7d28fd9c0f124a6bae47b8d6
sovlookup <gonorth@qq.com>
2024-07-18 20:14:52 +0800

    adapter to cloudflare

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/docs/',
+  base: '/',
   title: "rubick 文档",
   description: "rubick 官方文档",
   head: [
